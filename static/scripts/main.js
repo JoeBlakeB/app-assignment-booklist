98094976cd9e59025bdb1a10d02a0ad4bcddf86c
Joe Baker <joeblakeb@gmail.com>
2022-03-04 11:50:40 +0000

    added change layout and the exit settings function but not the button yet

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -10,14 +10,33 @@ var currentPage = "none";
 // param    isbn    the book to open (optional, 0 if not a book)
 function openPage(page, isbn=0) {
     if (page == currentPage) { return }
-    if (page == "new" || page == "edit") {
+    if (currentPage == "new" || currentPage == "edit") {
         if (!confirm ("Are you sure?")) { return }
     }
     currentPage = page;
+    showDetailsContainer(page != "none");
     switch (page) {
+        case "none":
+            return document.getElementById("detailsContainer").innerHTML = "";
         case "settings":
             return settings.openSettingsPage();
         default:
             return;
+    }
+}
+
+// Add the showDetailsContainer class to body so that only one
+// container is displayed on mobile mode
+function showDetailsContainer(addClass) {
+    // Return if nothing needs to be added/removed
+    var alreadyAdded = document.body.className.includes("showDetailsContainer");
+    if ((addClass && alreadyAdded) || (!addClass && !alreadyAdded)) { return }
+    // Add showDetailsContainer to body classes
+    else if (addClass) {
+        document.body.className = document.body.className + " showDetailsContainer";
+    }
+    // Remove showDetailsContainer from the body class
+    else {
+        document.body.classList.remove("showDetailsContainer");
     }
 }
